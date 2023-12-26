c2bcdf5bf6d6fa5e504fb04ebdcdd9d104a9abf0
YebinYun <122279587+YebinYun@users.noreply.github.com>
2023-12-26 09:51:41 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,15 @@
+// 출력 조건
+// \    /\
+//  )  ( ')
+// (  /  )
+//  \(__)|
+
+#include <stdio.h>
+
+int main() {
+	printf("\\    /\\\n");
+	printf(" )  ( ')\n");
+	printf("(  /  ) \n");
+	printf(" \\(__)|\n");
+	return 0;
+}
