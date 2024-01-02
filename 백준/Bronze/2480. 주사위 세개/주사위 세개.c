ac5d8bc6cc323a1b34e3a253d50e0443fff13226
YebinYun <122279587+YebinYun@users.noreply.github.com>
2024-01-02 20:39:58 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.c
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 
-int getMax(int x, int y) {
-    return x > y ? x : y;
-}
+#define max(x, y) ((x) > (y) ? (x) : (y))
 
 int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
-
     if (a == b && b == c)
-        printf("%d", 10000 + a * 1000);
+        printf("%d", 10000 + a*1000);
     else if (a == b || a == c)
-        printf("%d", 1000 + a * 100);
+        printf("%d", 1000 + a*100);
     else if (b == c)
-        printf("%d", 1000 + b * 100);
+        printf("%d", 1000 + b*100);
     else
-       printf("%d", getMax(getMax(a, b), c) * 100);
-
+        printf("%d", max(max(a, b), c)*100);
     return 0;
 }
