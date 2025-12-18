d918e720bd76e9ff4a9409ed2e95eb2b4d234e36
YebinYun <122279587+YebinYun@users.noreply.github.com>
2025-12-18 22:49:46 +0900

    [Silver IV] Title: 괄호, Time: 100 ms, Memory: 9608 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.js
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -5,19 +5,21 @@ const count = Number(input[0]);
 
 for (let i = 1; i <= count; i++) {
   const command = input[i];
-  const stack = [];
   let isValid = true;
+  const stack = [];
 
   for (let char of command) {
+    if (stack.length === 0 && char === ")") {
+      isValid = false;
+      break;
+    }
+
     if (char === "(") {
       stack.push(char);
     } else if (char === ")") {
-      if (stack.length === 0) {
-        isValid = false;
-        break;
-      }
       stack.pop();
     }
   }
+
   console.log(stack.length === 0 && isValid ? "YES" : "NO");
 }
