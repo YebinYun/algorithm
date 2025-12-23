e7d7808cf3093dd58c29d4ba43f71f15d123e205
YebinYun <122279587+YebinYun@users.noreply.github.com>
2025-12-23 21:26:49 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 320 ms, Memory: 61380 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const input = fs.readFileSync(0, "utf8").trim().split(/\s+/);
 
-const N = parseInt(input[0]);
-const M = parseInt(input[1]);
+const N = Number(input[0]);
+const M = Number(input[1]);
 
 const numbers = input.slice(2, 2 + N).map(Number);
 const prefix = new Array(N + 1).fill(0);
@@ -15,8 +15,8 @@ let result = "";
 let index = 2 + N;
 
 for (let q = 0; q < M; q++) {
-  const start = parseInt(input[index++]);
-  const end = parseInt(input[index++]);
+  const start = Number(input[index++]);
+  const end = Number(input[index++]);
   const sum = prefix[end] - prefix[start - 1];
   result += sum + "\n";
 }
